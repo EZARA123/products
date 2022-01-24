c2dc5ee2815f71d3241e6688d45f735b9d0e88c5
orpa <orpa@fresnou.org>
2022-01-24 08:54:12 -0800

    Have An Error

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const productController = require('../controllers/productController.js')
-
+const reviewController = require('../controllers/reviewController.js')
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -11,6 +11,9 @@ router.get('/products/profile/:id', productController.renderProfile);
 router.get('/products/edit/:id', productController.renderEditForm);
 router.get('/products/add', productController.renderAddForm);
 router.post('/products/edit/:id', productController.updateProduct);
-router.get('/products/delete/:id', productController.deleteProduct);
 router.post('/products/add', productController.addProduct);
+router.get('/products/delete/:id', productController.deleteProduct);
+
+router.get('/products/:productId/review/add', reviewController.renderAddReviewForm)
+router.post('/products/:productId/review/add', reviewController.addReview)
 module.exports = router;
